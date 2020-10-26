40f060da6fc1e0e018a0dc203de8b73735195f79
Raswanth Rajangam <59132948+Raswanth8@users.noreply.github.com>
2020-10-26 14:25:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function shadowpad(options){
+    let images = document.querySelectorAll('.shadowpad');
+
+    if (options.shadow_type === 'hard'){
+        options.shadow_type = '0px'
+    }
+
+    else {
+        options.shadow_type = '15px'
+    }
+
+    images.forEach(image => {
+        image.style.boxshadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+
+        if(options.padding){
+            image.style.padding = '1 em';
+        }
+    })
+    
+}
+
+module.exports.shadowpad = shadowpad;
